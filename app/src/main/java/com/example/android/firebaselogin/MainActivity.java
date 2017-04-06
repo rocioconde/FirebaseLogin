d0269c92863ec0faf13c1f0ad316d545766880f5
rocionde <rec2146@tc.columbia.edu>
2017-04-06 15:23:59 -0400

    Still not showing

--- app/src/main/java/com/example/android/firebaselogin/MainActivity.java
+++ app/src/main/java/com/example/android/firebaselogin/MainActivity.java
@@ -1,13 +1,115 @@
 package com.example.android.firebaselogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseAuth.AuthStateListener authListener;
+    private TextView displayText;
+    private EditText textToAdd;
+    private DatabaseReference userRef = database.getReference();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayText = (TextView) findViewById(R.id.display_text);
+        textToAdd = (EditText) findViewById(R.id.write_something);
+
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null)
+                    startActivity(new Intent(MainActivity.this, LogInActivity.class));
+                else {
+                    userRef.addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                    textToAdd.add(dataSnapshot.getValue());
+//                            displayString();
+                        }
+
+                        @Override
+                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                            Toast.makeText(MainActivity.this, dataSnapshot.getValue() + " has changed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onChildRemoved(DataSnapshot dataSnapshot) {
+                            Toast.makeText(MainActivity.this, dataSnapshot.getValue() + " is removed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
+
+                }
+            }
+
+
+        };
+
+    }
+
+//    private void displayString() {
+//        String text = "";
+//        for (String s: )
+//        {
+//            text += s + "\n";
+//        }
+//        displayText.setText(text);
+//    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authListener);
+    }
+
+    public void sendSomething(View view) {
+
+        FirebaseUser user = auth.getCurrentUser();
+        DatabaseReference userRef = database.getReference(user.getUid());
+        userRef.push().setValue(textToAdd.getText().toString());
+    }
+
+    public void signOut(View view) {
+        auth.signOut();
+
     }
 }
